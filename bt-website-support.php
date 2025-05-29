cce65930023ababcacd75dfd360ff63d71824631
John Stuifbergen <johnstuif@gmail.com>
2025-05-29 15:17:48 -0500

    Uncommented the auto-updates code. D'oh

--- bt-website-support.php
+++ bt-website-support.php
@@ -16,8 +16,8 @@ if (!defined('ABSPATH')) {
 
 
 require_once plugin_dir_path(__FILE__) . 'includes/admin-dashboard-support-form.php';
-// require_once plugin_dir_path(__FILE__) . 'includes/check-for-updates.php';
-// require_once plugin_dir_path(__FILE__) . 'includes/enable-auto-updates.php';
+require_once plugin_dir_path(__FILE__) . 'includes/check-for-updates.php';
+require_once plugin_dir_path(__FILE__) . 'includes/enable-auto-updates.php';
 require_once plugin_dir_path(__FILE__) . 'includes/remove-dashboard-widgets.php';
 require_once plugin_dir_path(__FILE__) . 'includes/reset-dashboard-widgets.php';
 
