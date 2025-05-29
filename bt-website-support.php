7ba2922d59faf0f2fa4095981d8ca2507e95882d
John Stuifbergen <johnstuif@gmail.com>
2025-05-29 15:17:48 -0500

    Uncommented the auto-updates code. D'oh

--- bt-website-support.php
+++ bt-website-support.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Buildertrend Pro Websites Support
 Description: Our required plugin for website support. 
-Version: 1.0.2
+Version: 1.0.3
 Author: BT Pro Websites Team
 License: GPLv2 or later
 Plugin URI:        https://github.com/buildertrend/bt-website-support
@@ -16,8 +16,8 @@ if (!defined('ABSPATH')) {
 
 
 require_once plugin_dir_path(__FILE__) . 'includes/admin-dashboard-support-form.php';
-// require_once plugin_dir_path(__FILE__) . 'includes/check-for-updates.php';
-// require_once plugin_dir_path(__FILE__) . 'includes/enable-auto-updates.php';
+require_once plugin_dir_path(__FILE__) . 'includes/check-for-updates.php';
+require_once plugin_dir_path(__FILE__) . 'includes/enable-auto-updates.php';
 require_once plugin_dir_path(__FILE__) . 'includes/remove-dashboard-widgets.php';
 require_once plugin_dir_path(__FILE__) . 'includes/reset-dashboard-widgets.php';
 
